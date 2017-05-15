d31b559c302434bd9450e09ff1075fc89601bb11
ShiYanJie <syj91@qq.com>
2017-05-15 15:11:33 +0800

    首次提交

--- app/src/main/java/com/shouba/shouba/ui/shouba/ShouBaBangFragment.java
+++ app/src/main/java/com/shouba/shouba/ui/shouba/ShouBaBangFragment.java
@@ -4,7 +4,7 @@ import com.jaydenxiao.common.base.BaseFragment;
 import com.shouba.shouba.R;
 
 /**
- * des:瘦吧的分类的fragment  瘦吧榜
+ * des:瘦吧的分类的fragment 瘦吧榜
  * Created by syj
  * on 2017.05.09 10:30
  */
