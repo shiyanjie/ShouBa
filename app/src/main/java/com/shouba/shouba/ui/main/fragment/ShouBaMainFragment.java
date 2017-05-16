d898a75dbdd9b9c77677f433e70c3395fd7db160
ShiYanJie <syj91@qq.com>
2017-05-16 17:20:48 +0800

    首次提交

--- app/src/main/java/com/shouba/shouba/ui/main/fragment/ShouBaMainFragment.java
+++ app/src/main/java/com/shouba/shouba/ui/main/fragment/ShouBaMainFragment.java
@@ -1,13 +1,16 @@
 package com.shouba.shouba.ui.main.fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.jaydenxiao.common.base.BaseFragment;
 import com.jaydenxiao.common.base.BaseFragmentAdapter;
@@ -18,6 +21,7 @@ import com.shouba.shouba.ui.shouba.ShouBaBangFragment;
 import com.shouba.shouba.ui.shouba.ShouBaQuanFragment;
 import com.shouba.shouba.util.MyUtils;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,6 +66,13 @@ public class ShouBaMainFragment extends BaseFragment{
         fragmentAdapter = new BaseFragmentAdapter(getChildFragmentManager(), mNewsFragmentList, channelNames);
         viewPager.setAdapter(fragmentAdapter);
         tabs.setupWithViewPager(viewPager);
+        //设置TabLayout子空间间距和下划线的长度,必须在TabLayout渲染后设置
+        tabs.post(new Runnable() {
+            @Override
+            public void run() {
+                setIndicator(tabs,10,10);
+            }
+        });
         MyUtils.dynamicSetTabLayoutMode(tabs);
         setPageChangeListener();
 
@@ -94,4 +105,42 @@ public class ShouBaMainFragment extends BaseFragment{
 
         return rootView;
     }
+
+    /*
+    * 设置TabLayout子空间间距和下划线的长度
+    * */
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        tabStrip.setAccessible(true);
+        LinearLayout llTab = null;
+        try {
+            llTab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+        for (int i = 0; i < llTab.getChildCount(); i++) {
+            View child = llTab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
+
+
+    }
+
+
 }
